f4eac3dd6aec426ebd49f112a55b4b6ea97242f2
Donovan Isherwood <donovan@matacore.co.za>
2017-04-15 16:30:53 +0200

    Allowed for additional params to be passed onto the url

--- index.js
+++ index.js
@@ -13,7 +13,54 @@ const isValidSourceDest = (args) => {
 
 function getDirections (args) {
   if (!isValidSourceDest(args)) {
+    return Promise.reject(new Error('Invalid arguments provided'))'use strict'
+
+import { Linking } from 'react-native'
+
+const isValidLatLong = (num, range) => typeof num === 'number' && num <= range && num >= (-1 * range)
+const isValidCoordinates = (coords) => isValidLatLong(coords.latitude, 90) && isValidLatLong(coords.longitude, 180)
+
+const isValidSourceDest = (args) => {
+  const validSource = args.source && isValidCoordinates(args.source)
+  const vaidDestination = args.destination && isValidCoordinates(args.destination)
+  return vaidDestination && validSource
+}
+
+const getParameterString = (params) => {
+  let paramsStr = ""
+
+  if (!params || !params.map) return paramsStr;
+
+  params.map(({ key, value}) => {
+    paramsStr += `&${key}=${value}`
+  });
+
+  return paramsStr
+}
+
+function getDirections (args) {
+  if (!isValidSourceDest(args)) {
     return Promise.reject(new Error('Invalid arguments provided'))
+  }
+
+  const {destination, source, params} = args
+  const paramsStr = getParameterString(params);
+
+  const url = `http://maps.google.com/maps?saddr=${source.latitude},${source.longitude}&daddr=${destination.latitude},${destination.longitude}${paramsStr}`
+
+  console.log(url);
+
+  return Linking.canOpenURL(url).then((supported) => {
+    if (!supported) {
+      return Promise.reject(new Error(`Could not open the url: ${url}`))
+    } else {
+      return Linking.openURL(url)
+    }
+  })
+}
+
+export default getDirections
+
   }
 
   const {destination, source} = args
