2469a56f8d87627b4fa828cc6ca1d1636396b41f
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2019-05-12 12:25:36 +0200

    Update deps & run lint

--- index.js
+++ index.js
@@ -1,60 +1,57 @@
-import { Linking } from 'react-native';
+import { Linking } from 'react-native'
 
 const isValidLatLong = (num, range) =>
-  typeof num === 'number' && num <= range && num >= -1 * range;
+  typeof num === 'number' && num <= range && num >= -1 * range
 
 const isValidCoordinates = coords =>
-  isValidLatLong(coords.latitude, 90) && isValidLatLong(coords.longitude, 180);
+  isValidLatLong(coords.latitude, 90) && isValidLatLong(coords.longitude, 180)
 
 const getParameterString = (params = []) => {
   return params
     .map(({ key, value }) => {
-      const encodedKey = encodeURIComponent(key);
-      const encodedValue = encodeURIComponent(value);
-      return `${encodedKey}=${encodedValue}`;
+      const encodedKey = encodeURIComponent(key)
+      const encodedValue = encodeURIComponent(value)
+      return `${encodedKey}=${encodedValue}`
     })
-    .join('&');
-};
-
-const getWaypointsString = (waypoints = []) =>{
-  return waypoints.map(value=>{
-      return `${value.latitude},${value.longitude}`;
-    }).join('|')
-  
+    .join('&')
 }
 
-function getDirections({ destination, source, params = [] ,waypoints = []} = {}) {
+const getWaypointsString = (waypoints = []) => {
+  return waypoints.map(value => {
+    return `${value.latitude},${value.longitude}`
+  }).join('|')
+}
+
+function getDirections ({ destination, source, params = [], waypoints = [] } = {}) {
   if (destination && isValidCoordinates(destination)) {
     params.push({
       key: 'destination',
-      value: `${destination.latitude},${destination.longitude}`,
-    });
+      value: `${destination.latitude},${destination.longitude}`
+    })
   }
   if (source && isValidCoordinates(source)) {
     params.push({
       key: 'origin',
-      value: `${source.latitude},${source.longitude}`,
-    });
+      value: `${source.latitude},${source.longitude}`
+    })
   }
-   if (source && isValidCoordinates(source)) {
+  if (source && isValidCoordinates(source)) {
     params.push({
       key: 'origin',
-      value: `${source.latitude},${source.longitude}`,
-    });
+      value: `${source.latitude},${source.longitude}`
+    })
   }
-
-
 
   const url = `https://www.google.com/maps/dir/?api=1&${getParameterString(
     params
-  )}&waypoints=${getWaypointsString(waypoints)}`;
+  )}&waypoints=${getWaypointsString(waypoints)}`
   return Linking.canOpenURL(url).then(supported => {
     if (!supported) {
-      return Promise.reject(new Error(`Could not open the url: ${url}`));
+      return Promise.reject(new Error(`Could not open the url: ${url}`))
     } else {
-      return Linking.openURL(url);
+      return Linking.openURL(url)
     }
-  });
+  })
 }
 
-export default getDirections;
+export default getDirections
